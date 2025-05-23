66036acc2ab6eb09d8f1c8d8cf46487c627fdda0
brian <giudicelli.be@gmail.com>
2025-05-23 06:21:49 -0300

    feat: main view design with scroll pane and menu bar

--- src/main/java/org/udemy/javafxudemy/HelloApplication.java
+++ src/main/java/org/udemy/javafxudemy/HelloApplication.java
@@ -2,7 +2,9 @@ package org.udemy.javafxudemy;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        Scene scene = new Scene(scrollPane, 1600, 900);
+        stage.setTitle("JavaFX Basic Application");
         stage.setScene(scene);
         stage.show();
     }
